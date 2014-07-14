247af279deae7f374e1044a718d1e0b3dd935439
Caleb Crosby <caleb@cicsolutions.com>
2014-07-13 20:41:48 -0600

    Add AutoEmbed Library
    - this does all the real heavy lifting!
    - started to build out the init() method, but not sure how best to test
    my work from the runtime folder.

--- AutoEmbed.php
+++ AutoEmbed.php
@@ -4,9 +4,15 @@ namespace cics\widgets;
 
 class AutoEmbed extends \yii\base\Widget
 {
+	public $url;
+	
 	public function init()
 	{
 		parent::init();
+		
+		if ($this->url === null) {
+			return 'Please pass a URL parameter into the widget';
+		}
 	}
 	
     public function run()
