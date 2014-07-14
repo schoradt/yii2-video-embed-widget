d23063aac86de776c7d820acf8cfa70a457ee47d
Caleb Crosby <caleb@cicsolutions.com>
2014-07-13 23:30:34 -0600

    Finish & Clean Main Class File
    - Really slimmed down, and working like a dream!

--- AutoEmbed.php
+++ AutoEmbed.php
@@ -1,34 +1,34 @@
 <?php
 
+/**
+ * @copyright Copyright &copy; CICSolutions, CICSolutions.com, 2014
+ * @package yii2-video-embed-widget
+ * @version 1.0.0
+ */
+
 namespace cics\widgets;
-use Yii;
+
+use yii\helpers\Html;
 
 class AutoEmbed extends \yii\base\Widget
 {
 	public $url;
-	public $embed_code;
-	
+
     public function run()
     {
-
+    	// make sure a source url was provided
 		if ($this->url === null)
 			return 'Please pass a URL parameter into the widget';
-			    	
-	    // include the autoembed class
-/* 		include(__DIR__ . '/lib/AutoEmbed-1.8/AutoEmbed.class.php'); */
-	    Yii::$classMap['AutoEmbedGen'] = (__DIR__ . '/lib/AutoEmbed-1.8/AutoEmbed.class.php');
-	    $ae = new AutoEmbedGen();
-	    
-	    // try to generate embed code from passed url
-/*
-		Yii::$classMap['myclassname'] = __DIR__ . '/lib/AutoEmbed-1.8/AutoEmbed.class.php';
-		$ae = new myclassname();
-*/
-		
-		// load the embed source from a remote url
-/* 		if (!$ae->parseUrl($this->url)) */
-			return "Embed code could not be generated for this URL ({$this->url})";
 
-        return $this->embed_code;
+	    // include embed class
+		include_once(__DIR__ . '/../../../vendor/embed/embed/Embed/autoloader.php');
+
+	    // look up data on url
+	    $data = \Embed\Embed::create($this->url);
+
+	    if (!is_object($data) || is_null($data->code))
+	    	return "Embed code could not be generated for this URL ({$this->url})";
+
+        return $data->code;
     }
 }
