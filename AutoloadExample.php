a74169001363e469c4c5e5b999ab3448560ec226
Caleb Crosby <caleb@cicsolutions.com>
2014-07-13 20:02:11 -0600

    Start Init function
    very basic - really just need a commit for pushing through to packagist
    so i can try to install via composer.

--- AutoloadExample.php
+++ AutoloadExample.php
@@ -4,6 +4,11 @@ namespace cics\widgets;
 
 class AutoloadExample extends \yii\base\Widget
 {
+	public function init()
+	{
+		parent::init();
+	}
+	
     public function run()
     {
         return "Hello!";
