e154d3b83b4c88a5d3dc680a9c7a446f6cbd0fa3
Caleb Crosby <caleb@cicsolutions.com>
2014-07-13 21:58:45 -0600

    Start Include of Library
    - started using AutoEmbed, but think I’ve found a better one that uses
    composer.
    - trying the composer route :)

--- AutoEmbed.php
+++ AutoEmbed.php
@@ -1,22 +1,34 @@
 <?php
 
 namespace cics\widgets;
+use Yii;
 
 class AutoEmbed extends \yii\base\Widget
 {
 	public $url;
-	
-	public function init()
-	{
-		parent::init();
-		
-		if ($this->url === null) {
-			return 'Please pass a URL parameter into the widget';
-		}
-	}
+	public $embed_code;
 	
     public function run()
     {
-        return "Hello!";
+
+		if ($this->url === null)
+			return 'Please pass a URL parameter into the widget';
+			    	
+	    // include the autoembed class
+/* 		include(__DIR__ . '/lib/AutoEmbed-1.8/AutoEmbed.class.php'); */
+	    Yii::$classMap['AutoEmbedGen'] = (__DIR__ . '/lib/AutoEmbed-1.8/AutoEmbed.class.php');
+	    $ae = new AutoEmbedGen();
+	    
+	    // try to generate embed code from passed url
+/*
+		Yii::$classMap['myclassname'] = __DIR__ . '/lib/AutoEmbed-1.8/AutoEmbed.class.php';
+		$ae = new myclassname();
+*/
+		
+		// load the embed source from a remote url
+/* 		if (!$ae->parseUrl($this->url)) */
+			return "Embed code could not be generated for this URL ({$this->url})";
+
+        return $this->embed_code;
     }
 }
