c1ebdbaff7d50e6899ef43e6d8fd35b7f9ac139b
Caleb Crosby <caleb@cicsolutions.com>
2014-07-17 17:58:26 -0600

    Update Readme
    - add better param formatting

--- VideoEmbed.php
+++ VideoEmbed.php
@@ -21,13 +21,13 @@ class VideoEmbed extends \yii\base\Widget
     public function run()
     {
     	// make sure a source url was provided
-		if (is_null($this->url) || $this->url == '')
+		if (is_null($this->url) || empty($this->url))
 			return $this->show_errors ? 'Please pass a URL parameter to scan for a video to embed.' : false;
 
 	    // include embed class
 		include_once(__DIR__ . '/../../../vendor/embed/embed/Embed/autoloader.php');
 
-	    // look up data on url
+	    // look up data for the supplied url
 	    $data = \Embed\Embed::create($this->url);
 		
 		// make sure we received a video embed code
